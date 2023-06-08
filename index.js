80cef86e98402d813f56d1ce8c8fe30c9863701e
Rafsanfaruk <39384467+Rafsanfaruk@users.noreply.github.com>
2023-06-09 02:17:20 +0600

    data get form mongodb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 // const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -12,7 +13,7 @@ app.use(express.json());
 // mongodb start
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.luzjykj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +29,49 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const ourClassesCollection = client.db("danceDb").collection("category");
+    const classesCollection = client.db("danceDb").collection("danceClasses");
+    const instructorsCollection = client.db("danceDb").collection("instructors");
+
+   
+    app.get('/ourClasses', async (req, res) => {
+      try {
+        // Retrieve data from the collection
+        const result = await ourClassesCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error('Error retrieving data:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+
+    app.get('/classes', async (req, res) => {
+      try {
+        const result = await classesCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error('Error fetching classes:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    app.get('/instructors', async (req, res) => {
+      try {
+        const result = await instructorsCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error('Error fetching instructors:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
